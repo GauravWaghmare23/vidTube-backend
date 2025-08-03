aba32f57b1c2f066a6a082afd794bd760f12e3bb
Gaurav Waghmare <gauravwaghmare95032.com>
2025-08-03 19:17:44 +0530

    .

---
+++ src/models/user.models.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    avatar: {
+      type: String, // Cloudinary URL
+    },
+    coverImage: {
+      type: String, // Cloudinary URL
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
